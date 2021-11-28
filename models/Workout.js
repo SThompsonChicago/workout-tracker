b9a014c534f171dc367e8671a56e5efd8b270fc2
Stephen Thompson <s31@umbc.edu>
2021-11-28 16:17:45 -0600

    created models

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                require: "Enter the type of exercise",
+            },
+            name: {
+                type: String,
+                trim: true,
+                require: "Enter the name of the exercise",
+            },
+            duration: {
+                type: Number,
+                required: "Enter the duration",
+            },
+            distance: {
+                type: Number,
+                default: 0,
+            },
+            reps: {
+                type: Number,
+                default: 0,
+            },
+            sets: {
+                type: Number,
+                default: 0,
+            },
+            weight: {
+                type: Number,
+                default: 0,
+            },
+        },
+    ],
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
