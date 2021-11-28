03f3502b550bb8cbc4a1254d18714e51277e2973
Stephen Thompson <s31@umbc.edu>
2021-11-28 17:23:19 -0600

    started routes and got server running

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use(require('./routes/api.js'));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useFindAndModify: false
+ });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
     console.log(`Workout Tracker running on port ${PORT}`);
