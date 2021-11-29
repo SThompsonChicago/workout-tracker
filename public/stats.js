8bca8e1213f8559022cd33a78954cdcc420e5b8a
Stephen Thompson <s31@umbc.edu>
2021-11-28 20:33:35 -0600

    reverted to older stats file

--- public/stats.js
+++ public/stats.js
@@ -15,26 +15,8 @@ function calculateTotalWeight(data) {
   return totals;
 }
 
-function calculateTotalDuration(data) {
-  const totals = [];
-
-  data.forEach((workout) => {
-    const workoutTotal = workout.exercises.reduce((total, duration) => {
-      if (true) {
-        return total + duration;
-      }
-      return total;
-    }, 0);
-
-    totals.push(workoutTotal);
-  });
-
-  return totals;
-}
-
-
 function populateChart(data) {
-  const durations = calculateTotalDuration(data);
+  const durations = data.map(({ totalDuration }) => totalDuration);
   const pounds = calculateTotalWeight(data);
 
   const line = document.querySelector('#canvas').getContext('2d');
